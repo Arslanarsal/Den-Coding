8293dca77acf2cc8042c4e568974aa9d1a5787e3
Arslan <arslanarsal455@gmail.com>
2024-11-10 17:27:15 +0500

    add

--- A.cpp
+++ A.cpp
@@ -1,29 +1,124 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
 
 using namespace std;
-using namespace __gnu_pbds;
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oSet; // [find_by_order ==> given address at index] |==| [order_of_key ==> Number of element smaller then X]y
-#define int long long
-#define ld long double
-const int mod = 1e9 + 7;
-int t, n, m, q, x;
 
-int32_t main()
+// Step 1: Define the 3D distance matrix with the shortest route distances for each city pair
+const int INF = INT_MAX;
+const int CITIES = 4;
+const int ROUTES = 5;
+int distances[CITIES][CITIES][ROUTES] = {
+    {// Islamabad
+     {0, 0, 0, 0, 0},
+     {300, 300, 300, 300, 300},
+     {250, 250, 250, 250, 250},
+     {500, 500, 500, 500, 500}},
+    {// Lahore
+     {300, 300, 300, 300, 300},
+     {20, 38, 51, 11, 8},
+     {89, 92, 101, 42, 39},
+     {150, 172, 158, 111, 62}},
+    {// Gujranwala
+     {250, 250, 250, 250, 250},
+     {89, 92, 101, 42, 39},
+     {11, 15, 9, 2, 4},
+     {69, 71, 77, 89, 85}},
+    {// Karachi
+     {500, 500, 500, 500, 500},
+     {150, 172, 158, 111, 62},
+     {69, 71, 77, 89, 85},
+     {22, 28, 59, 51, 72}}};
+
+// Step 2: Compute the minimum distance matrix
+int min_distances[CITIES][CITIES];
+
+// Initialize the minimum distances
+void initializeMinDistances()
 {
-   fastio;
-    t = 1;
-    // cin >> t;
-    while (t--)
+    for (int i = 0; i < CITIES; ++i)
     {
-        cin >> n;
-        
+        for (int j = 0; j < CITIES; ++j)
+        {
+            int min_dist = INF;
+            for (int k = 0; k < ROUTES; ++k)
+            {
+                min_dist = min(min_dist, distances[i][j][k]);
+            }
+            min_distances[i][j] = min_dist;
+        }
+    }
+}
+
+// Step 3: Solve TSP with bitmasking and memoization
+unordered_map<int, unordered_map<int, int>> memo;
+
+int tsp(int pos, int visited, const vector<int> &cities)
+{
+    if (visited == (1 << cities.size()) - 1)
+    {
+        return min_distances[pos][0]; // Return to the starting city (Islamabad)
     }
 
+    if (memo[pos].count(visited))
+        return memo[pos][visited];
+
+    int min_cost = INF;
+    for (int i = 0; i < cities.size(); ++i)
+    {
+        if (!(visited & (1 << i)))
+        {
+            int next_city = cities[i];
+            int cost = min_distances[pos][next_city] + tsp(next_city, visited | (1 << i), cities);
+            min_cost = min(min_cost, cost);
+        }
+    }
+
+    return memo[pos][visited] = min_cost;
+}
+
+// Step 4: Find and print the optimal route
+void findOptimalRoute(string start_city, vector<string> cities_to_visit)
+{
+    // Map city names to indices
+    unordered_map<string, int> city_indices = {
+        {"Islamabad", 0},
+        {"Lahore", 1},
+        {"Gujranwala", 2},
+        {"Karachi", 3}};
+
+    int start = city_indices[start_city];
+    vector<int> cities;
+    for (const auto &city : cities_to_visit)
+    {
+        cities.push_back(city_indices[city]);
+    }
+
+    // Initialize minimum distances and memoization table
+    initializeMinDistances();
+    memo.clear();
+
+    // Compute the minimum distance using TSP
+    int min_distance = tsp(start, 1 << start, cities);
+
+    // Display the results
+    cout << "Optimal Route: " << start_city;
+    for (const auto &city : cities_to_visit)
+    {
+        cout << " -> " << city;
+    }
+    cout << " -> " << start_city << endl;
+    cout << "Minimum Distance: " << min_distance << " km" << endl;
+    cout << "Thank you for using the City Route Planner. Safe travels!" << endl;
+}
+
+int main()
+{
+    // Example usage
+    string start_city = "Islamabad";
+    vector<string> cities_to_visit = {"Lahore", "Gujranwala"};
+    findOptimalRoute(start_city, cities_to_visit);
     return 0;
 }
--- C.cpp
+++ C.cpp
@@ -1,29 +1,127 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
+#include <iostream>
+#include <unordered_map>
+#include <string>
 using namespace std;
-using namespace __gnu_pbds;
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oSet; // [find_by_order ==> given address at index] |==| [order_of_key ==> Number of element smaller then X]y
-#define int long long
-#define ld long double
-const int mod = 1e9 + 7;
-int t, n, m, q, x;
 
-int32_t main()
+class LRUCache
 {
-   fastio;
-    t = 1;
-    // cin >> t;
-    while (t--)
+public:
+    class ListNode
     {
-        cin >> n;
-        
+    public:
+        int cacheKey;
+        string cacheValue;
+        ListNode *nextNode, *prevNode;
+        ListNode(int cacheKey, string cacheValue)
+        {
+            this->cacheKey = cacheKey;
+            this->cacheValue = cacheValue;
+            nextNode = NULL;
+            prevNode = NULL;
+        }
+    };
+
+    int maxCapacity;
+    unordered_map<int, ListNode *> cacheMap;
+    ListNode *front = new ListNode(-1, "");
+    ListNode *rear = new ListNode(-1, "");
+
+    LRUCache(int maxCapacity)
+    {
+        this->maxCapacity = maxCapacity;
+        front->nextNode = rear;
+        rear->prevNode = front;
+        cout << "Initialized cache with capacity " << maxCapacity << endl;
     }
+
+    void removeNode(ListNode *node)
+    {
+        node->prevNode->nextNode = node->nextNode;
+        node->nextNode->prevNode = node->prevNode;
+    }
+
+    void insertToFront(ListNode *node)
+    {
+        ListNode *tempNode = front->nextNode;
+        front->nextNode = node;
+        node->prevNode = front;
+        node->nextNode = tempNode;
+        tempNode->prevNode = node;
+    }
+
+    string fetch(int key)
+    {
+        if (cacheMap.find(key) == cacheMap.end())
+            return "-1";
+        else
+        {
+            ListNode *currentNode = cacheMap[key];
+            removeNode(currentNode);
+            insertToFront(currentNode);
+            cout << "Fetch " << key << ": \"" << currentNode->cacheValue << "\" -> Cache updated to ";
+            displayCache();
+            return currentNode->cacheValue;
+        }
+    }
+
+    void add(int key, string value)
+    {
+        if (cacheMap.find(key) != cacheMap.end())
+        {
+            ListNode *existingNode = cacheMap[key];
+            existingNode->cacheValue = value;
+            removeNode(existingNode);
+            insertToFront(existingNode);
+        }
+        else
+        {
+            ListNode *newNode = new ListNode(key, value);
+            if (cacheMap.size() == maxCapacity)
+            {
+                ListNode *toDelete = rear->prevNode;
+                cacheMap.erase(toDelete->cacheKey);
+                removeNode(toDelete);
+                insertToFront(newNode);
+                cacheMap[key] = newNode;
+                delete toDelete;
+            }
+            else
+            {
+                insertToFront(newNode);
+                cacheMap[key] = newNode;
+            }
+        }
+        cout << "Add " << key << ": Cache = ";
+        displayCache();
+    }
+
+    void displayCache()
+    {
+        ListNode *current = front->nextNode;
+        cout << "[";
+        while (current != rear)
+        {
+            cout << "\"" << current->cacheValue << "\"";
+            if (current->nextNode != rear)
+                cout << ", ";
+            current = current->nextNode;
+        }
+        cout << "]" << endl;
+    }
+};
+
+int main()
+{
+    LRUCache cache(3);
+    cout << "\n";
+    cache.add(1, "Data1");
+    cache.add(2, "Data2");
+    cache.fetch(1);
+    cache.add(3, "Data3");
+    cache.add(4, "Data4");
+    cout << "Fetch 2: " << cache.fetch(2) << endl;
+    cache.displayCache();
+     cout << "\n";
 
     return 0;
 }
